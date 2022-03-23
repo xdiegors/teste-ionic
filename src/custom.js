4197693d22385b38ea98f5b25ae9d6ee013eeb91
xdiegors <xdiegors@outlook.com>
2022-03-23 13:54:52 -0300

    Lanterna ok

--- src/custom.js
+++ src/custom.js
@@ -1,50 +1,60 @@
-function lanterna() {
+//have a console on mobile
+const consoleOutput = document.getElementById("console");
+const log = function (msg) {
+  consoleOutput.innerText = `${consoleOutput.innerText}\n${msg}`;
+  console.log(msg);
+}
 
 //Test browser support
+const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
 
-  const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
+if (SUPPORTS_MEDIA_DEVICES) {
+  //Get the environment camera (usually the second one)
+  navigator.mediaDevices.enumerateDevices().then(devices => {
 
-  if (SUPPORTS_MEDIA_DEVICES) {
-    //Get the environment camera (usually the second one)
-    navigator.mediaDevices.enumerateDevices().then(devices => {
+    const cameras = devices.filter((device) => device.kind === 'videoinput');
 
-      const cameras = devices.filter((device) => device.kind === 'videoinput');
-
-      if (cameras.length === 0) {
-        throw 'No camera found on this device.';
+    if (cameras.length === 0) {
+      log('No camera found on this device.');
+    }
+    // Create stream and get video track
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: 'environment',
       }
-      const camera = cameras[cameras.length - 1];
+    }).then(stream => {
+      const track = stream.getVideoTracks()[0];
 
-      // Create stream and get video track
-      navigator.mediaDevices.getUserMedia({
-        video: {
-          deviceId: camera.deviceId,
-          facingMode: ['user', 'environment'],
-          height: {ideal: 1080},
-          width: {ideal: 1920}
-        }
-      }).then(stream => {
-        const track = stream.getVideoTracks()[0];
+      //Create image capture object and get camera capabilities
+      const imageCapture = new ImageCapture(track)
+      imageCapture.getPhotoCapabilities().then(capabilities => {
+        //let there be light!
+        const btn = document.querySelector('.switch');
+        const torchSupported = !!capabilities.torch || (
+          'fillLightMode' in capabilities &&
+          capabilities.fillLightMode.length != 0 &&
+          capabilities.fillLightMode != 'none'
+        );
 
-        //Create image capture object and get camera capabilities
-        const imageCapture = new ImageCapture(track)
-        const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
-
-          //todo: check if camera has a torch
-
-          //let there be light!
-          const btn = document.querySelector('.switch');
-          btn.addEventListener('click', function(){
-            track.applyConstraints({
-              advanced: [{torch: true}]
-            });
+        if (torchSupported) {
+          let torch = false;
+          btn.addEventListener('click', function (e) {
+            try {
+              track.applyConstraints({
+                advanced: [{
+                  torch: (torch = !torch)
+                }]
+              });
+            } catch (err) {
+              log(err);
+            }
           });
-        });
-      });
-    });
+        } else {
+          log("No torch found");
+        }
+      }).catch(log);
+    }).catch(log);
+  }).catch(log);
 
-    //The light will be on as long the track exists
-
-
-  }
+  //The light will be on as long the track exists
 }
